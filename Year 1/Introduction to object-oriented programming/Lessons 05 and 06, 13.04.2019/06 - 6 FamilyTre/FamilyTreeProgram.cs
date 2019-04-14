3aeb616b8d060c9f087224fe6d495fab2f7809e3
Syndamia <kami02882@gmail.com>
2019-04-14 16:07:59 +0300

    Dragon has been defeated! Exercises 6 from lessons 6 is solved, also added comments that explain some of the code

--- Year 1/Introduction to object-oriented programming/Lessons 05 and 06, 13.04.2019/06 - 6 FamilyTre/FamilyTreeProgram.cs
+++ Year 1/Introduction to object-oriented programming/Lessons 05 and 06, 13.04.2019/06 - 6 FamilyTre/FamilyTreeProgram.cs
@@ -15,17 +15,21 @@ namespace _06___6_FamilyTre
             string memberInfo = Console.ReadLine();
             if (memberInfo.Contains('/'))
             {
-                int[] mInfo = memberInfo.Where(x => !x.Equals('/')).Select(x => int.Parse(x + "")).ToArray();
+                int[] mInfo = memberInfo.Split('/').Select(x => int.Parse(x + "")).ToArray();
+
                 family.Add(new Person(new DateTime(mInfo[2], mInfo[1], mInfo[0])));
+                family.Single().ToBePrinted = true;
             }
-            else family.Add(new Person(memberInfo));
+            else
+            {
+                family.Add(new Person(memberInfo));
+                family.Single().ToBePrinted = true;
+            }
 
             while (true)
             {
                 string[] info = Console.ReadLine().Split(' ').ToArray();
                 int[] dates = InfoToDatesArray(info);
-
-                 
 
                 if (info[0] == "End") break;
 
@@ -33,18 +37,26 @@ namespace _06___6_FamilyTre
                 {
                     Person tempPerson = new Person(info[0] + " " + info[1], new DateTime(dates[2], dates[1], dates[0]));
 
-                    if(family.Where(x => x.BirthDate == tempPerson.BirthDate).Count() > 0 &&
-                        family.Where(x => x.Name == tempPerson.Name).Count() > 0)
+                    //if we have two "people", i.e. one with only a name and other with only a birth date (and both with children and parents), 
+                    //and we find out that they are one person, we need to merge them into one, so the name, birth date, children and parents are all in one place 
+                    if(family.Count(x => x.BirthDate == tempPerson.BirthDate) > 0 &&
+                                 family.Count(x => x.Name == tempPerson.Name) > 0)
                     {
-                        family.Find(x => x.Name == tempPerson.Name).Merge(family.Find(x => x.BirthDate == tempPerson.BirthDate));
-                        family.RemoveAll(x => x.BirthDate == tempPerson.BirthDate && x.Name == null);
+                        family.Find(x => x.Name == tempPerson.Name).Merge(family.Find(x => x.BirthDate == tempPerson.BirthDate), family);
                     }
 
-                    else if (family.Where(x => x.BirthDate == tempPerson.BirthDate).Count() > 0)
-                    { family.Find(p => p.BirthDate == tempPerson.BirthDate).Name = tempPerson.Name; }
+                    //searches for a person, whose birthdate matches the inputed birthdate, 
+                    //i.e. checkes if there is any person that has that birth date, and then sets their name to the inputed one
+                    //ifs like that work on the same principle
+                    else if (family.Count(x => x.BirthDate == tempPerson.BirthDate) > 0)
+                    {
+                        family.Find(p => p.BirthDate == tempPerson.BirthDate).Name = tempPerson.Name;
+                    }
 
-                    else if (family.Where(x => x.Name == tempPerson.Name).Count() > 0)
-                    { family.Find(p => p.Name == tempPerson.Name).BirthDate = tempPerson.BirthDate; }
+                    else if (family.Count(x => x.Name == tempPerson.Name) > 0)
+                    {
+                        family.Find(p => p.Name == tempPerson.Name).BirthDate = tempPerson.BirthDate;
+                    }
 
                     else family.Add(tempPerson); 
                 }
@@ -57,10 +69,10 @@ namespace _06___6_FamilyTre
                             Person tempPerson1 = new Person(new DateTime(dates[2], dates[1], dates[0]));
                             Person tempPerson2 = new Person(new DateTime(dates[5], dates[4], dates[3]));
 
-                            if (family.Where(p => p.BirthDate == tempPerson1.BirthDate).Count() == 0)
+                            if (family.Count(p => p.BirthDate == tempPerson1.BirthDate) == 0)
                             { family.Add(tempPerson1); }
 
-                            if(family.Where(p => p.BirthDate == tempPerson2.BirthDate).Count() == 0)
+                            if(family.Count(p => p.BirthDate == tempPerson2.BirthDate) == 0)
                             { family.Add(tempPerson2); }
 
                             family.Find(p => p.BirthDate == tempPerson1.BirthDate).Children.Add(family.Find(p => p.BirthDate == tempPerson2.BirthDate));
@@ -71,10 +83,10 @@ namespace _06___6_FamilyTre
                             Person tempPerson1 = new Person(new DateTime(dates[2], dates[1], dates[0]));
                             Person tempPerson2 = new Person(info[2] + " " + info[3]);
 
-                            if (family.Where(p => p.BirthDate == tempPerson1.BirthDate).Count() == 0)
+                            if (family.Count(p => p.BirthDate == tempPerson1.BirthDate) == 0)
                             { family.Add(tempPerson1); }
 
-                            if(family.Where(p => p.Name == tempPerson2.Name).Count() == 0)
+                            if(family.Count(p => p.Name == tempPerson2.Name) == 0)
                             { family.Add(tempPerson2); }
 
                             family.Find(p => p.BirthDate == tempPerson1.BirthDate).Children.Add(family.Find(p => p.Name == tempPerson2.Name));
@@ -86,10 +98,10 @@ namespace _06___6_FamilyTre
                         Person tempPerson1 = new Person(info[0] + " " + info[1]);
                         Person tempPerson2 = new Person(new DateTime(dates[2], dates[1], dates[0]));
                         
-                        if (family.Where(p => p.Name == tempPerson1.Name).Count() == 0)
+                        if (family.Count(p => p.Name == tempPerson1.Name) == 0)
                         { family.Add(tempPerson1); }
 
-                        if (family.Where(p => p.BirthDate == tempPerson2.BirthDate).Count() == 0)
+                        if (family.Count(p => p.BirthDate == tempPerson2.BirthDate) == 0)
                         { family.Add(tempPerson2); }
 
                         family.Find(p => p.Name == tempPerson1.Name).Children.Add(family.Find(p => p.BirthDate == tempPerson2.BirthDate));
@@ -100,10 +112,10 @@ namespace _06___6_FamilyTre
                         Person tempPerson1 = new Person(info[0] + " " + info[1]);
                         Person tempPerson2 = new Person(info[3] + " " + info[4]);
 
-                        if (family.Where(p => p.Name == tempPerson1.Name).Count() == 0)
+                        if (family.Count(p => p.Name == tempPerson1.Name) == 0)
                         { family.Add(tempPerson1); }
 
-                        if (family.Where(p => p.Name == tempPerson2.Name).Count() == 0)
+                        if (family.Count(p => p.Name == tempPerson2.Name) == 0)
                         { family.Add(tempPerson2); }
 
                         family.Find(p => p.Name == tempPerson1.Name).Children.Add(family.Find(p => p.Name == tempPerson2.Name));
@@ -112,19 +124,21 @@ namespace _06___6_FamilyTre
                 }
             }
 
-            Console.WriteLine(family[0].ToString());
+            Console.WriteLine(family.Find(p => p.ToBePrinted).ToString());
         }
 
         private static int[] InfoToDatesArray(string[] info)
         {
-            try
+            try //the code splits the dates into integers, but if the input doesn't have dates, i.e. Pesho Peshev - Gancho Peshev, code will "blow"
             {
+                //in case there are 2 dates, it makes them into one date, for example: 11/11/1951 - 23/5/1980 will be turned into 11/11/1951/23/5/1980
                 string datesString = string.Join("/", info.Where(x => x.Contains('/')));
+                //splits the "mega dates" (when two dates are combined, but there could be only 1 date) into integers
                 int[] dates = datesString.Split('/').Select(int.Parse).ToArray();
                 return dates;
             }
             catch (Exception)
-            { return null; }
+            { return null; } //return null, because we don't have dates and will not need dates (case Име Фамилия - Име Фамилия)
         }
     }
 }
--- Year 1/Introduction to object-oriented programming/Lessons 05 and 06, 13.04.2019/06 - 6 FamilyTre/Person.cs
+++ Year 1/Introduction to object-oriented programming/Lessons 05 and 06, 13.04.2019/06 - 6 FamilyTre/Person.cs
@@ -14,31 +14,51 @@ namespace _06___6_FamilyTre
         private List<Person> parents;
         private List<Person> children;
 
-        public void Merge (Person secondPerson)
+        private bool toBePrinted;
+
+        public void Merge (Person secondPerson, List<Person> family)
         {
+            if (secondPerson.ToBePrinted) this.ToBePrinted = true;
+
             birthDate = secondPerson.BirthDate;
 
             parents.AddRange(secondPerson.Parents);
             children.AddRange(secondPerson.Children);
-            secondPerson.Parents.Select(p => p.Children.Remove(secondPerson)).ToArray();
-            
-            secondPerson.Children.Select(c => c.Parents.Remove(secondPerson)).ToArray();
+
+            //remove the old "person" and add the new, combined, person into the parents's list of children
+            foreach(var parent in secondPerson.Parents)
+            {
+                parent.Children.Remove(secondPerson);
+                parent.Children.Add(this);
+            }
+
+            //remove the old "person" and add the new, combined, person into the children's list of parents
+            foreach (var child in children)
+            {
+                child.Parents.Remove(secondPerson);
+                child.Parents.Add(this);
+            }
+
+            family.Remove(secondPerson);
         }
 
         public Person()
         {
+            ToBePrinted = false;
             Parents = new List<Person>();
             Children = new List<Person>();
         }
 
         public Person(string name) : this(name, new DateTime())
         {
+            toBePrinted = false;
             Parents = new List<Person>();
             Children = new List<Person>();
         }
 
         public Person(DateTime birthDate) : this(null, birthDate)
         {
+            toBePrinted = false;
             Parents = new List<Person>();
             Children = new List<Person>();
         }
@@ -47,6 +67,7 @@ namespace _06___6_FamilyTre
         {
             Name = name;
             BirthDate = birthDate;
+            toBePrinted = false;
             Parents = new List<Person>();
             Children = new List<Person>();
         }
@@ -54,22 +75,28 @@ namespace _06___6_FamilyTre
         public override string ToString()
         {
             StringBuilder toReturn = new StringBuilder();
-            toReturn.Append($"{Name} {BirthDate.Date}\r\nParents:\r\n");
+            toReturn.Append($"{Name} {DateToString(BirthDate)}\r\nParents:\r\n");
 
             foreach(var parent in parents)
-            { toReturn.Append($"{parent.Name} {parent.BirthDate.Date}\r\n"); }
+            { toReturn.Append($"{parent.Name} {DateToString(parent.BirthDate)}\r\n"); }
 
             toReturn.Append($"Children:\r\n");
 
             foreach(var child in children)
-            { toReturn.Append($"{child.Name} {child.BirthDate.Date}\r\n"); }
+            { toReturn.Append($"{child.Name} {DateToString(child.BirthDate)}\r\n"); }
 
             return toReturn.ToString();
+        }
+
+        private string DateToString(DateTime birthDate)
+        {
+            return $"{birthDate.Day}/{birthDate.Month}/{birthDate.Year}";
         }
 
         public string Name { get => name; set => name = value; }
         public DateTime BirthDate { get => birthDate; set => birthDate = value; }
         public List<Person> Parents { get => parents; set => parents = value; }
         public List<Person> Children { get => children; set => children = value; }
+        public bool ToBePrinted { get => toBePrinted; set => toBePrinted = value; }
     }
 }
